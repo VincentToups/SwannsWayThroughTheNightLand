c45f2aafb04b7d2bbb91c9c341bd89b4d71a4349
Vincent Toups <vincent.toups@gmail.com>
2014-11-13 16:56:21 +0100

    Added custom trained word vectors, html generation.

--- README.md
+++ README.md
@@ -5,21 +5,12 @@ new novel by substituting similar sentences from one novel for the
 sentences of another.  The structure of the first is preserved, but
 the content becomes like the second.
 
-See the file output/swanns-way-through-the-nightland.txt for an
+See the file output/swanns-way-through-the-night-land.txt for an
 example.
 
-You can download the file vectors.txt from here:
-
-http://procyonic.org/vectors.txt
-
-You'll have to change the line:
-
-    (load-word-vectors "/scratch/vectors.txt")
-
-To point to wherever you download this file, if you want to run the
-project.
-
-[Word2Vec](https://code.google.com/p/word2vec/) was used to produce this file.  
+[Word2Vec](https://code.google.com/p/word2vec/) was used to produce
+word vectors based on the data set created by concatenating the words
+from [Swann's Way](http://en.wikipedia.org/wiki/In_Search_of_Lost_Time) with those of [The Night Land](http://en.wikipedia.org/wiki/The_Night_Land).  
 
 This technique is a textual transposition of my previous generative
 art-work: [Meat is Mulder](http://procyonic.org/meatIsMulder/meatIsMulder.html).
--- generate-a-novel.el
+++ generate-a-novel.el
@@ -3,6 +3,7 @@
   nil)
 
 (defvar vectors nil) ;; will contain the word2vec vectors
+(defvar word-vector-length nil)
 (defun load-word-vectors (filename) 
   "Given a text based output file from word2vec, load the vectors
 into a hash table indexed by the symbol for each word."
@@ -10,16 +11,17 @@ into a hash table indexed by the symbol for each word."
 		(lines (with-current-buffer (find-file-noselect filename) (prog1 (cdr (buffer-all-lines)) (kill-buffer)))))
 	(loop while (car lines) 
 		  do
-		  (let* ((line (pop lines))
-				 (res (read-from-string line))
-				 (key (car res))
-				 (vec (car (read-from-string (concat "[" (substring line (cdr res)) "]")))))
+		  (let* ((line-parts (split-string (pop lines) " "))
+				 (key (intern (car line-parts)))
+				 (vec (car (read-from-string (concat "[" (join (cdr line-parts)) "]")))))
 			(puthash key vec tbl)))
-	(setf vectors tbl)))
+	(setf vectors tbl)
+	(setf word-vector-length (length (gethash (car (keyshash tbl)) tbl)))))
 
 ;; Load the word2vec vectors into memory and get their length
-(load-word-vectors "/scratch/vectors.txt")
-(defvar word-vector-length (length (gethash 'and vectors)))
+;;(load-word-vectors "/scratch/vectors.txt")
+(load-word-vectors "./resources/swtnl-vectors.txt")
+
 
 (defun cos-dist (v1 v2)
   "Given two equal-length vectors, return a number between 0 and
@@ -81,6 +83,9 @@ are as different as possible."
 						   (aset vec i (+ (aref vec i) (aref wvec i)))))))
 	vec))
 
+(example 
+ (sentence->vector "Hello world"))
+
 (defun collect-sentence-data (file-name)
   "Given a file name, extract all the sentences from the file and
 calculate their vectors.  Return the result as a list."
@@ -98,12 +103,12 @@ calculate their vectors.  Return the result as a list."
 						((start (car bnds))
 						 (end (cdr bnds))
 						 (sentence (buffer-substring-no-properties start end))
-						 (svec (sentence->vector sentence)))
+						 (sentence-vector (sentence->vector sentence)))
 					  (make-sentence-data :sentence sentence
 									 :source file-name
 									 :start start
 									 :end end
-									 :vector svec)) sentences)
+									 :vector sentence-vector)) sentences)
 			  (forward-thing 'sentence)
 			  (setq bnds (bounds-of-thing-at-point 'sentence))
 			  finally 
@@ -111,23 +116,71 @@ calculate their vectors.  Return the result as a list."
 						(let* ((start (car bnds))
 							   (end (cdr bnds))
 							   (sentence (buffer-substring-no-properties start end))
-							   (svec (sentence->vector sentence)))
+							   (sentence-vector (sentence->vector sentence)))
 						  (make-sentence-data :sentence sentence
 											  :source file-name
 											  :start start
 											  :end end
-											  :vector svec)) 
+											  :vector sentence-vector)) 
 						sentences)))
 		(reverse sentences)))))
 
-(defun nearest-sentence (s from-set)
+(defun dump-file-words (infile outfile)
+  (let ((out-buffer (find-file-noselect outfile))) 
+	(flet ((output (&rest args)
+				   (with-current-buffer out-buffer 
+					 (apply #'insert args))))
+	  (with-current-buffer out-buffer
+		(kill-region (point-min) (point-max)))
+	  (with-current-buffer (find-file-noselect infile)
+		(save-excursion 
+		  (goto-char (point-min))
+		  (forward-word) ;; goto the first word's end
+		  (let ((bnds (bounds-of-thing-at-point 'word)))
+			(loop while (and bnds
+							 (not (>= (cdr bnds) (point-max)))) 
+				  
+				  do 
+				  (output (downcase (buffer-substring-no-properties (car bnds) (cdr bnds))))
+				  (output " ")
+				  (forward-thing 'word)
+				  (setq bnds (bounds-of-thing-at-point 'word))
+				  finally 
+				  (if bnds (downcase (output (buffer-substring-no-properties (car bnds) (cdr bnds)))))))))
+	  (with-current-buffer out-buffer (save-buffer)))))
+
+
+;; Define a circular buffer to hold excluded sentences.  We'll use an
+;; array to minimize consing.
+(cl-defstruct circular-buffer array index)
+
+(defun circular-buffer-insert (ca val)
+  "Insert a value into the circular buffer."
+  (let ((i (circular-buffer-index ca))
+		(array (circular-buffer-array ca))) 
+	(aset array i val)
+	(setf (circular-buffer-index ca)
+		  (mod (+ i 1) (length array)))))
+
+(defun in-array (obj array test)
+  "Returns T when OBJ is in the array ARRAY by TEST"
+  (let ((result nil))
+	(loop for item across array until (progn (setq result (funcall test item obj)) result))
+	result))
+
+(defun in-circular-buffer (obj cb test)
+  "Same as IN-ARRAY but for a circular buffer."
+  (in-array obj (circular-buffer-array cb) test))
+
+(defun nearest-sentence (s from-set exclude)
   "Given a sentence S and a set FROM-SET of sentences, find the best match."
   (let ((svec (sentence-data-vector s))
 		(best nil)
 		(best-score 2.0))
 	(loop for ss in from-set do
 		  (let ((d (cos-dist svec (sentence-data-vector ss))))
-			(if (< d best-score)
+			(if (and (< d best-score)
+					 (not (in-circular-buffer ss exclude #'eq)))
 				(progn 
 				  (setq best ss)
 				  (setq best-score d)))))
@@ -145,14 +198,17 @@ with their closest matches from SOURCE-FILE and store the result
 in OUTPUT-FILE-NAME."
   (let ((template-data (collect-sentence-data template-file))
 		(source-data (collect-sentence-data source-file))
-		(template-txt (get-file-contents template-file)))
+		(template-txt (get-file-contents template-file))
+		;; we exclude from use the last ten used sentences.
+		(exlude (make-circular-buffer :array (make-vector 10 :no-match) :index 0)))
 	(with-current-buffer (find-file-noselect output-file-name)
 	  (delete-region (point-min) (point-max))
 	  (insert template-txt)
 	  (goto-char (point-min))
 	  (let ((current-offset 0)) 
 	  	(loop for s in template-data do 
-	  		  (let ((best-match (nearest-sentence s source-data)))
+	  		  (let ((best-match (nearest-sentence s source-data exlude)))
+				(circular-buffer-insert exlude best-match)
 				(goto-char (+ current-offset (sentence-data-end s)))
 	  			(delete-region (+ current-offset (sentence-data-start s))
 	  						   (+ current-offset (sentence-data-end s)))
@@ -160,6 +216,40 @@ in OUTPUT-FILE-NAME."
 	  			(setq current-offset 
 	  				  (+ current-offset (- (sentence-data-length best-match)
 	  									   (sentence-data-length s)))))))
+	  (save-buffer))))
+
+(defun perform-substitution->html (template-file source-file output-file-name)
+  "Perform a substitution of all sentences from TEMPLATE-FILE
+with their closest matches from SOURCE-FILE and store the result
+in OUTPUT-FILE-NAME.  This version generates an HTML file with
+tool-tips showing the source sentence for each sentence, thus
+revealing that the almost random results."
+  (let ((template-data (collect-sentence-data template-file))
+		(source-data (collect-sentence-data source-file))
+		(template-txt (get-file-contents template-file))
+		(exlude (make-circular-buffer :array (make-vector 10 :no-match) :index 0)))
+	(with-current-buffer (find-file-noselect output-file-name)
+	  (delete-region (point-min) (point-max))
+	  (insert template-txt)
+	  (goto-char (point-min))
+	  (let ((current-offset 0)) 
+	  	(loop for s in template-data do 
+	  		  (let ((best-match (nearest-sentence s source-data exlude)))
+				(circular-buffer-insert exlude best-match)
+				(goto-char (+ current-offset (sentence-data-end s)))
+	  			(delete-region (+ current-offset (sentence-data-start s))
+	  						   (+ current-offset (sentence-data-end s)))
+	  			(let ((to-insert (format "<span title='%S'>%s</span>" 
+										 (replace-regexp-in-string "[[:cntrl:]]+" " " (sentence-data-sentence s)) 
+										 (sentence-data-sentence best-match)))) 
+				  (insert to-insert)
+				 (setq current-offset 
+					   (+ current-offset (- (length to-insert)
+											(sentence-data-length s))))))))
+	  (goto-char (point-min)) 
+	  (insert "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title></title></head><body>")
+	  (goto-char (point-max))
+	  (insert "</body></html>")
 	  (save-buffer))))
 
 (defun matrix (template-file source-file)
@@ -181,13 +271,20 @@ in OUTPUT-FILE-NAME."
 
  ;; Generate a novel.
  (perform-substitution "./resources/tnl.txt" "./resources/swannsway.txt" "./output/swanns-way-through-the-night-land.txt")
+
+ ;; Generate a novel.
+ (perform-substitution->html "./resources/tnl.txt" "./resources/bible.txt" "./output/kjv-night-land.txt")
  
+ (perform-substitution->html "./resources/tnl.txt" "./resources/swannsway.txt" "./output/swanns-way-through-the-night-land.html")
+
+ (dump-file-words "./resources/tnl.txt" "./resources/tnl-words.txt")
+ (dump-file-words "./resources/swannsway.txt" "./resources/swanns-way-words.txt")
+
+ (perform-substitution "./resources/tnl.txt" "./resources/swannsway.txt" "./output/swanns-way-through-the-night-land-custom-vectors-exclude-repeats.txt")
+ (perform-substitution->html "./resources/tnl.txt" "./resources/swannsway.txt" "./output/swanns-way-through-the-night-land-custom-vectors-exclude-repeats.html")
+
+ (gethash (intern "it's") vectors)
  ;; An example of extracting sentence data.
  (collect-sentence-data "./resources/template.txt"))
-
-
-
-
-
 
 
